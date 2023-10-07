b7b0877366c0482acf1ad69fd30a52904eb98b31
jpablopachar <jpablopachar1993@gmail.com>
2023-10-06 23:55:45 -0500

    Validación para controlar primera letra mayúscula

--- Entities/Author.cs
+++ Entities/Author.cs
@@ -1,17 +1,14 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+using app_authors.Validations;
 
 namespace app_authors.Entities
 {
     public class Author
     {
         public int Id { get; set; }
-        [Required(ErrorMessage ="El campo {0} es requerido")]
-        [StringLength(5, ErrorMessage ="El campo {0} no debe tener mas de {1} caracteres")]
-        [PrimeraLetraMayuscula]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(5, ErrorMessage = "El campo {0} no debe tener mas de {1} caracteres")]
+        [FirstCapitalLetter]
         public string? Name { get; set; }
         public List<AuthorBook>? BookAuthors { get; set; }
     }
---
+++ Validations/FirstCapitalLetter.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace app_authors.Validations
+{
+    public class FirstCapitalLetter : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString())) return ValidationResult.Success!;
+
+            var firstLetter = value.ToString()![0].ToString();
+
+            if (firstLetter != firstLetter.ToUpper()) return new ValidationResult("La primera letra debe ser mayúscula");
+
+            return ValidationResult.Success!;
+        }
+    }
+}
